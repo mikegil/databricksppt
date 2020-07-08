115aaba7fab8ee09f6656bfa51207a5a096856f5
Michael Gilbert <michael.r.gilbert@me.com>
2020-07-08 18:09:38 -0500

    Progress on PPT

--- databricksppt/databricksppt.py
+++ databricksppt/databricksppt.py
@@ -1,16 +1,90 @@
 from pathlib import Path
+from os import path
+from pptx import Presentation
+from pptx.util import Inches
+from pptx.enum.shapes import PP_PLACEHOLDER
+import pandas as pd
 
 
-def toPPT(df):
-    lines = ""
-    with open(get_templatefile_name()) as template:
-        for line in template:
-            lines += line
-    return lines
+def toPPT(df, template=None, layout=1, title=None, subtitle="", slideNum=0):
+    if (not isinstance(df, pd.DataFrame)):
+        return None
+
+    if (template is not None):
+        if (not isinstance(template, str)):
+            template = None
+        else:
+            if (not template == "" and not path.isfile(template)):
+                template = None
+
+    pres = Presentation(template)
+    slide = None
+
+    if slideNum == 0:
+        slide = pres.slides.add_slide(pres.slide_layouts[1])
+    else:
+        if len(pres.slides) >= slideNum:
+            slide = pres.slides[slideNum-1]
+        else:
+            return None
+
+    #placeholder = slide.shapes[1]
+
+    if len(slide.placeholders) == 0:
+        return None
+
+    placeholderIdx = []
+
+    for shape in slide.placeholders:
+        placeholderIdx.append(shape.placeholder_format.idx)
+
+    shape = slide.placeholders[placeholderIdx[0]]
+    phf = shape.placeholder_format
+    if phf.type == PP_PLACEHOLDER.TITLE:
+        if len(slide.placeholders) < 2:
+            return
+        else:
+            __set_titles(shape, title, subtitle)
+            shape = slide.placeholders[placeholderIdx[1]]
+
+    print('%d, %s' % (phf.idx, phf.type))
+    print('%d, %d, %d, %d' % (shape.left, shape.top, shape.width, shape.height))
+
+    x, y, cx, cy = Inches(2), Inches(2), Inches(4), Inches(1.5)
+    colNames = df.columns.tolist()
+
+    # table = placeholder.insert_table(cols=df.shape[0], rows=df.shape[1])
+
+    # col = 0
+    # for colName in colNames:
+    #     table.cell(0, col).text = colName
+    #     col += 1
+
+    # for index, rows in df.iterrows():
+    #     col = 0
+    #     for colName in colNames:
+    #         table.cell(index+1, col).text = colName
+    #         col += 1
+
+    return pres
 
 
-def get_templatefile_name():
+def __set_titles(titleph, title, subtitle):
+    if not titleph.has_text_frame:
+        return
+    if title is None or not isinstance(title, str):
+        return
+
+    text_frame = titleph.text_frame
+    p = text_frame.paragraphs[0]
+    p.text = title
+    if (len(text_frame.paragraphs) > 1 and subtitle is not None and isinstance(subtitle, str)):
+        p = text_frame.paragraphs[1]
+        p.text = subtitle
+
+
+def __get_datafile_name(filename):
     """
     return the default template file that comes with the package
     """
-    return Path(__file__).parent / "data/template.html"
+    return Path(__file__).parent / "data/" + filename
--- databricksppt/main.py
+++ databricksppt/main.py
@@ -2,36 +2,36 @@
 
 import sys
 import os
+import click
+import numpy as np
+import pandas as pd
+from pathlib import Path
 from databricksppt import databricksppt
-
-help = """
-databricksppt Script
-
-databricksppt file_to_process [output_file_name]
-
-Creates databricksppt file from data
-"""
+from pptx import Presentation
 
 
-def main():
+@click.command()
+@click.argument('inputfile', type=click.Path(exists=True, dir_okay=False, resolve_path=True))
+@click.argument('outputfile', type=click.Path())
+@click.option('--template', type=click.Path(exists=True, dir_okay=False, resolve_path=True), help='Create PPTX from given template')
+@click.option('--layout', type=int, help='Layout within template for new slide')
+@click.option('--title', type=str, help='Title for slide on which to place data')
+@click.option('--subtitle', type=str, help='Subtitle for slide on which to place data')
+@click.option('--slide', type=int, default=0, help='Slide # on which to place data; 0 = new slide (default)')
+@click.option('--open', is_flag=True)
+def main(inputfile, outputfile, template, layout, title, subtitle, slide, open):
     """
     startup function for running databricksppt as a script
     """
-    try:
-        infilename = sys.argv[1]
-    except IndexError:
-        print("you need to pass in a file name to process")
-        print(help)
-        sys.exit()
-    try:
-        outfilename = sys.argv[2]
-    except IndexError:
-        root, ext = os.path.splitext(infilename)
-        outfilename = "test.html"
+    if (Path(outputfile).suffix != '.pptx'):
+        outputfile += '.pptx'
 
-    # do the real work:
-    print("Producing databricksppt from: %s and storing it in %s" %
-          (infilename, outfilename))
-    print(databricksppt.toPPT(""))  # infilename, outfilename)
-
-    print("done")
+    df = pd.read_csv(inputfile)
+    pres = databricksppt.toPPT(
+        df, template=template, layout=layout, title=title, subtitle=subtitle, slideNum=slide)
+    if (pres is not None):
+        pres.save(outputfile)
+        if open:
+            os.system('open '+outputfile)
+    else:
+        print("No PPT Created")
