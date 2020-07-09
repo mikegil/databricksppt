4b4f079cfa027c70e1567bf8ad5ff98411c4ec75
Michael Gilbert <michael.r.gilbert@me.com>
2020-07-09 18:01:11 -0500

    Basic Charts now work

--- databricksppt/databricksppt.py
+++ databricksppt/databricksppt.py
@@ -1,14 +1,71 @@
 from pathlib import Path
 from os import path
+from enum import Enum
+import numbers
+from collections.abc import Iterable
 from pptx import Presentation
-from pptx.util import Inches
+from pptx.enum.chart import XL_CHART_TYPE
+from pptx.chart.data import CategoryChartData, XyChartData, BubbleChartData
 from pptx.enum.shapes import PP_PLACEHOLDER
+from itertools import islice
 import pandas as pd
+import numpy as np
 
 
-def toPPT(df, template=None, layout=1, title=None, subtitle="", slideNum=0, chart_type='Table'):
-    if (not isinstance(df, pd.DataFrame)):
+class CHART_TYPE(Enum):
+    AREA = 'Area'
+    AREA_STACKED = 'Area-Stacked'
+    AREA_STACKED_100 = 'Area-Stacked-100'
+    BAR = 'Bar'
+    BAR_STACKED = 'Bar-Stacked'
+    BAR_STACKED_100 = 'Bar-Stacked-100'
+    COLUMN = 'Column'
+    COLUMN_STACKED = 'Column-Stacked'
+    COLUMN_STACKED_100 = 'Column-Stacked-100'
+    LINE = 'Line'
+    LINE_STACKED = 'Line-Stacked'
+    LINE_STACKED_100 = 'Line-Stacked-100'
+    LINE_MARKED = 'Line-Marked'
+    LINE_MARKED_STACKED = 'Line-Marked-Stacked'
+    LINE_MARKED_STACKED_100 = 'Line-Marked-Stacked-100'
+    DOUGHNUT = 'Doughnut'
+    DOUGHNUT_EXPLODED = 'Doughnut-Exploded'
+    PIE = 'Pie'
+    PIE_EXPLODED = 'Pie-Exploded'
+    RADAR = 'Radar'
+    RADAR_FILLED = 'Radar-Filled'
+    RADAR_MARKED = 'Radar-Marked'
+    XY_SCATTER = 'XY-Scatter'
+    XY_SCATTER_LINES = 'XY-Scatter-Lines'
+    XY_SCATTER_LINES_SMOOTHED = 'XY-Scatter-Lines-Smoothed'
+    XY_SCATTER_LINES_MARKED = 'XY-Scatter-Lines-Marked'
+    XY_SCATTER_LINES_MARKED_SMOOTHED = 'XY-Scatter-Lines-Marked-Smoothed'
+    BUBBLE = 'Bubble'
+    TABLE = 'Table'
+
+
+def chart_types():
+    values = []
+    for chart_type in CHART_TYPE:
+        values.append(chart_type.value)
+    return values
+
+
+def toPPT(dfs, template=None, layout=1, title=None, subtitle="", slideNum=0, chart_type='Table', transpose=False):
+    if (not isinstance(dfs, pd.DataFrame) and not __iterable(dfs)):
         return None
+
+    if (isinstance(dfs, pd.DataFrame)):
+        dfs = [dfs]
+    else:
+        for dataframe in dfs:
+            if not isinstance(dataframe, pd.DataFrame):
+                return None
+
+    if transpose:
+        dfs = __transpose_dfs(dfs)
+
+    df = dfs[0]
 
     if (template is not None):
         if (not isinstance(template, str)):
@@ -47,10 +104,102 @@ def toPPT(df, template=None, layout=1, title=None, subtitle="", slideNum=0, char
             __set_titles(placeholder, title, subtitle)
             placeholder = slide.placeholders[placeholderIdx[1]]
 
-    if chart_type == 'Table':
+    if chart_type == CHART_TYPE.AREA.value:
+        __insert_chart(XL_CHART_TYPE.AREA, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.AREA_STACKED.value:
+        __insert_chart(XL_CHART_TYPE.AREA_STACKED, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.AREA_STACKED_100.value:
+        __insert_chart(XL_CHART_TYPE.AREA_STACKED_100, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.BAR.value:
+        __insert_chart(XL_CHART_TYPE.BAR_CLUSTERED, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.BAR_STACKED.value:
+        __insert_chart(XL_CHART_TYPE.BAR_STACKED, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.BAR_STACKED_100.value:
+        __insert_chart(XL_CHART_TYPE.BAR_STACKED_100, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.COLUMN.value:
+        __insert_chart(XL_CHART_TYPE.COLUMN_CLUSTERED, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.COLUMN_STACKED.value:
+        __insert_chart(XL_CHART_TYPE.COLUMN_STACKED, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.COLUMN_STACKED_100.value:
+        __insert_chart(XL_CHART_TYPE.COLUMN_STACKED_100,
+                       slide, placeholder, df)
+    elif chart_type == CHART_TYPE.LINE.value:
+        __insert_chart(XL_CHART_TYPE.LINE, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.LINE_STACKED.value:
+        __insert_chart(XL_CHART_TYPE.LINE_STACKED, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.LINE_STACKED_100.value:
+        __insert_chart(XL_CHART_TYPE.LINE_STACKED_100, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.LINE_MARKED.value:
+        __insert_chart(XL_CHART_TYPE.LINE_MARKERS, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.LINE_MARKED_STACKED.value:
+        __insert_chart(XL_CHART_TYPE.LINE_MARKERS_STACKED,
+                       slide, placeholder, df)
+    elif chart_type == CHART_TYPE.LINE_MARKED_STACKED_100.value:
+        __insert_chart(XL_CHART_TYPE.LINE_MARKERS_STACKED_100,
+                       slide, placeholder, df)
+    elif chart_type == CHART_TYPE.DOUGHNUT.value:
+        __insert_chart(XL_CHART_TYPE.DOUGHNUT, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.DOUGHNUT_EXPLODED.value:
+        __insert_chart(XL_CHART_TYPE.DOUGHNUT_EXPLODED, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.PIE.value:
+        __insert_chart(XL_CHART_TYPE.PIE, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.PIE_EXPLODED.value:
+        __insert_chart(XL_CHART_TYPE.PIE_EXPLODED, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.RADAR.value:
+        __insert_chart(XL_CHART_TYPE.RADAR, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.RADAR_FILLED.value:
+        __insert_chart(XL_CHART_TYPE.RADAR_FILLED, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.RADAR_MARKED.value:
+        __insert_chart(XL_CHART_TYPE.RADAR_MARKERS, slide, placeholder, df)
+    elif chart_type == CHART_TYPE.XY_SCATTER.value:
+        __insert_xyzchart(XL_CHART_TYPE.XY_SCATTER, slide, placeholder, dfs)
+    elif chart_type == CHART_TYPE.XY_SCATTER_LINES.value:
+        __insert_xyzchart(XL_CHART_TYPE.XY_SCATTER_LINES_NO_MARKERS,
+                          slide, placeholder, dfs)
+    elif chart_type == CHART_TYPE.XY_SCATTER_LINES_SMOOTHED.value:
+        __insert_xyzchart(XL_CHART_TYPE.XY_SCATTER_SMOOTH_NO_MARKERS,
+                          slide, placeholder, dfs)
+    elif chart_type == CHART_TYPE.XY_SCATTER_LINES_MARKED.value:
+        __insert_chart(XL_CHART_TYPE.XY_SCATTER_LINES, slide, placeholder, dfs)
+    elif chart_type == CHART_TYPE.XY_SCATTER_LINES_MARKED_SMOOTHED.value:
+        __insert_xyzchart(XL_CHART_TYPE.XY_SCATTER_SMOOTH,
+                          slide, placeholder, dfs)
+    elif chart_type == CHART_TYPE.BUBBLE.value:
+        __insert_xyzchart(XL_CHART_TYPE.BUBBLE, slide, placeholder, dfs)
+    else:
         __insert_table(slide, placeholder, df)
 
+    BUBBLE = 'Bubble'
+
     return pres
+
+
+def __transpose_dfs(dfs):
+    tdfs = []
+    for dataframe in dfs:
+        labelsInFirstCol = True
+        firstCol = dataframe.iloc[:, 0]
+        for cell in firstCol:
+            if isinstance(cell, numbers.Number):
+                labelsInFirstCol = False
+
+        labelsInColumnHeaders = True
+        for col in dataframe.columns:
+            if isinstance(col, numbers.Number):
+                labelsInColumnHeaders = False
+
+        if labelsInFirstCol and labelsInColumnHeaders:
+            df1 = dataframe.set_index(
+                dataframe.columns[0]).transpose().reset_index()
+        elif labelsInColumnHeaders:
+            df1 = dataframe.transpose().reset_index()
+        else:
+            df1 = dataframe.set_index(dataframe.columns[0]).transpose()
+
+        tdfs.append(df1)
+#            tdfs.append(dataframe.set_index(dataframe.columns[0]).transpose())
+
+    return tdfs
 
 
 def __insert_table(slide, placeholder, df):
@@ -67,14 +216,101 @@ def __insert_table(slide, placeholder, df):
     # Populate table
     col = 0
     for colName in colNames:
-        table.cell(0, col).text = colName
+        table.cell(0, col).text = str(colName)
         col += 1
 
-    for index, rows in df.iterrows():
+    rowNum = 1
+    for index, row in df.iterrows():
         col = 0
         for colName in colNames:
-            table.cell(index+1, col).text = str(rows[col])
+            table.cell(rowNum, col).text = str(row.iloc[col])
             col += 1
+        rowNum += 1
+
+    return
+
+
+def __iterable(obj):
+    return isinstance(obj, Iterable)
+
+
+def __create_chartdata(df):
+    chart_data = CategoryChartData()
+
+    colNames = df.columns.tolist()
+
+    if len(colNames) >= 2:
+
+        for colName in colNames[1:]:
+            chart_data.categories.add_category(colName)
+
+        for index, row in df.iterrows():
+            data = []
+            for colName in colNames[1:]:
+                data.append(row[colName])
+
+            chart_data.add_series(str(row[0]), data)
+
+    return chart_data
+
+
+def __create_xyzdata(dfs):
+    chart_data = None
+
+    seriesNum = 1
+
+    for df in dfs:
+        colNames = df.columns.tolist()
+        name = 'Series ' + str(seriesNum)
+        if hasattr(df, 'name') and df.name != "":
+            name = df.name
+
+        if len(colNames) > 1 and len(colNames) < 4:
+            if len(colNames) == 2 and chart_data is None:
+                chart_data = XyChartData()
+            elif len(colNames) == 3 and chart_data is None:
+                chart_data = BubbleChartData()
+
+            series = chart_data.add_series(name)
+            for index, row in df.iterrows():
+                data = []
+                for colName in colNames:
+                    data.append(row[colName])
+
+                if len(colNames) == 2:
+                    series.add_data_point(data[0], data[1])
+                else:
+                    series.add_data_point(data[0], data[1], data[2])
+
+            seriesNum += 1
+
+    return chart_data
+
+
+def __insert_chart(chart_type, slide, placeholder, df):
+    chart_data = __create_chartdata(df)
+
+    # Create new element with same shape and position as placeholder
+    chart = slide.shapes.add_chart(chart_type, placeholder.left,
+                                   placeholder.top, placeholder.width, placeholder.height, chart_data).chart
+
+    # Remove empty placeholder
+    sp = placeholder._sp
+    sp.getparent().remove(sp)
+
+    return
+
+
+def __insert_xyzchart(chart_type, slide, placeholder, dfs):
+    chart_data = __create_xyzdata(dfs)
+
+    # Create new element with same shape and position as placeholder
+    chart = slide.shapes.add_chart(chart_type, placeholder.left,
+                                   placeholder.top, placeholder.width, placeholder.height, chart_data).chart
+
+    # Remove empty placeholder
+    sp = placeholder._sp
+    sp.getparent().remove(sp)
 
     return
 
--- databricksppt/main.py
+++ databricksppt/main.py
@@ -13,14 +13,16 @@ from pptx import Presentation
 @click.command()
 @click.argument('inputfile', type=click.Path(exists=True, dir_okay=False, resolve_path=True))
 @click.argument('outputfile', type=click.Path())
+@click.option('--inputfile2', type=click.Path(exists=True, dir_okay=False, resolve_path=True), help='Optional second data input file')
 @click.option('--template', type=click.Path(exists=True, dir_okay=False, resolve_path=True), help='Create PPTX from given template')
 @click.option('--layout', type=int, help='Layout within template for new slide')
 @click.option('--title', type=str, help='Title for slide on which to place data')
 @click.option('--subtitle', type=str, help='Subtitle for slide on which to place data')
 @click.option('--slide', type=int, default=0, help='Slide # on which to place data; 0 = new slide (default)')
-@click.option('--chart-type', type=click.Choice(['Table', 'Bar'], case_sensitive=False), default='Table', help='Type of chart to display (default = Table)')
+@click.option('--chart-type', type=click.Choice(databricksppt.chart_types(), case_sensitive=False), default='Table', help='Type of chart to display (default = Table)')
+@click.option('--transpose', is_flag=True, help='Switches the rows from the dataframe to be categories and the columns to be series')
 @click.option('--open', is_flag=True, help='Attempt to automatically open the PPTX file on success')
-def main(inputfile, outputfile, template, layout, title, subtitle, slide, chart_type, open):
+def main(inputfile, inputfile2, outputfile, template, layout, title, subtitle, slide, chart_type, transpose, open):
     """
     Runs databricksppt from the command line, using CSV input to produce a Powerpoint
     file including a Chart or Table built from this data
@@ -29,8 +31,13 @@ def main(inputfile, outputfile, template, layout, title, subtitle, slide, chart_
         outputfile += '.pptx'
 
     df = pd.read_csv(inputfile)
+    #df.name = "MyData"
+    if (inputfile2 is not None):
+        df2 = pd.read_csv(inputfile2)
+        df = [df, df2]
+
     pres = databricksppt.toPPT(
-        df, template=template, layout=layout, title=title, subtitle=subtitle, slideNum=slide, chart_type=chart_type)
+        df, template=template, layout=layout, title=title, subtitle=subtitle, slideNum=slide, chart_type=chart_type, transpose=transpose)
     if (pres is not None):
         pres.save(outputfile)
         if open:
