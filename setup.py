946b8a1d78dd939515caf1e2112be49c174fdad9
Michael Gilbert <michael.r.gilbert@me.com>
2020-09-01 14:20:50 -0500

    Bump version: 0.5.0-dev0 → 0.6.0-dev0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mikegil/databricksppt',
-    version='0.5.0-dev0',
+    version='0.6.0-dev0',
     zip_safe=False,
 )
